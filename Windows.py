727c71191c567c85dd612adf05eb40fa15f2baf7
Oguz Kayra <67963917+Mr-Plume@users.noreply.github.com>
2020-07-10 20:21:51 +0300

    Create Windows.py

---
+++ Windows.py
@@ -0,0 +1,128 @@
+def ban_system(): 
+  while True:
+    import time
+    import os
+    os.system("cls")
+    d = "type message"
+    for d in "type message":
+      import os 
+      import time
+      import sys
+      print(d, end='')
+      sys.stdout.flush()
+      time.sleep(0.2)
+    input1 = input(":")
+    if  "fuck" in input1.lower()  or  "bastard" in input1.lower()  or "bich" in input1.lower()  or "shit" in input1.lower()  or "ass" in input1.lower() :
+        print("detected level 1 swearing right now, shut up and sit")
+        time1 = 0
+        print("banned for 200 second")
+        time.sleep(1.5)
+        while time1 < 200:
+         import time
+         import os
+         time1 += 1
+         print("Ban Time: ",200-time1," Second")
+         time.sleep(1)
+         os.system("cls")
+        if time1 == 200:
+            print("end of banned")
+            time.sleep(1)
+            os.system("cls")
+
+
+    elif  "cock" in input1.lower()  or "anus" in input1.lower()  or "tit" in input1.lower()  or "piss" in input1.lower()  or "damn" in input1.lower() :
+        print("detected level 2 swearing so you banned system in 150 second")
+        time2 = 0
+        print("banned for 150 second")
+        time.sleep(1.5)
+        while time2 < 150:
+         import time
+         import os
+         time2 += 1
+         print("Ban Time: ",150-time2," Second")
+         time.sleep(1)
+         os.system("cls")
+        if time2 == 150:
+            print("end of banned")
+            time.sleep(1)
+            os.system("cls")
+
+    elif "bollocks" in input1.lower() or "bugger" in input1.lower(): 
+        print("detected level 3 swearing so shut up and wait")
+        time3 = 0
+        print("banned for 100 second")
+        time.sleep(2.0)
+        while time3 < 100:
+         import time
+         import os
+         time3 += 1
+         print("Ban Time: ",100-time3," Second")
+         time.sleep(1)
+         os.system("cls")
+         if time3 == 100:
+             print("end of banned")
+             time.sleep(1)
+             os.system("cls")
+
+    elif "choad" in input1.lower() or "crikey" in input1.lower() or "rubbish" in input1.lower() or "shag" in input1.lower():
+        print("detected level 4 swearing right now, relax because you angry")
+        time4 = 0
+        print("banned for 50 second")
+        time.sleep(2.0)
+        while time4 < 50:
+         import time
+         import os
+         time4 += 1
+         print("Ban Time: ",50-time4," Second")
+         time.sleep(1)
+         os.system("cls")
+         if time4 == 50:
+             print("end of banned")
+             time.sleep(1)
+             os.system("cls")
+      
+    elif "stupid" in input1.lower() or "idiot" in input1.lower() or "daft cow" in input1.lower() or "daftcow" in input1.lower():
+        import os
+        print("detected level 5 swearing right now, some be relax and dont type swearing")
+
+        print("banned just 10 second this time be relax")
+        time.sleep(10.0)
+        os.system("cls")
+    
+    else:
+        import os
+        print("user:",input1)
+        time.sleep(1)
+        os.system("cls")
+
+
+a = "Welcome To The 4chan"
+for a in "Welcome To The 4chan":
+    import os 
+    import time
+    import sys
+    print(a, end='')
+    sys.stdout.flush()
+    time.sleep(0.2)
+
+b = "do you want come?"
+for b in "\ndo you want come?":
+    import os 
+    import time
+    import sys
+    print(b, end='')
+    sys.stdout.flush()
+    time.sleep(0.2)
+
+answer = input("\n:")
+if "y" in answer.lower():
+  os.system("cls")
+  c = "connecting to the server"
+  for c in "connecting the server":
+    import sys
+    import time
+    import os
+    print(c, end='')
+    sys.stdout.flush()
+    time.sleep(0.2)
+  ban_system()
